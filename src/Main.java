694f1a14b12d02feaed89d5ca64c65e59d26aa63
Kevin Adams <adams.kevin@student.greenrive.edu>
2023-06-09 17:47:23 -0700

    Added docs to project

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
+/**
+ * this is my friday github lab
+ * @author Kevin
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * starts app
+     * @param args command line args (not used)
+     */
     public static void main(String[] args) {
         System.out.println("Hello world from SDEV 220!");
     }
